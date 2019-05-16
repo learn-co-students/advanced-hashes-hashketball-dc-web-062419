8504989eb198de87d0245055bf100984163e4f3b
Trevahr Robertson <trevahr@yahoo.com>
2019-05-16 22:23:31 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,17 +1,56 @@
-# Write your code here!
-
-
 def game_hash
 {
-  home: {team_name: "Team Name", colors: [], players: {"Player 1": {number:, shoe:, points:, rebounds:, assists:, blocks:, slam_dunks:}}}
+  home: {team_name: "Brooklyn Nets", 
+  colors: ["Black", "White"], 
+  players: 
+    {"Alan Anderson": 
+    {number: 0, 
+      shoe: 16, 
+    points: 22, 
+  rebounds: 12, 
+   assists: 12, 
+     steals: 3,
+     blocks: 1, 
+ slam_dunks: 1},
+ "Reggie Eveans": 
+    {number: 30, 
+       shoe: 14, 
+     points: 12, 
+   rebounds: 12, 
+    assists: 12, 
+     steals: 12,
+     blocks: 12, 
+  slam_dunks: 7},
+ "Brook Lopez": 
+    {number: 11, 
+       shoe: 17, 
+     points: 17, 
+   rebounds: 19, 
+    assists: 10, 
+     steals: 3,
+     blocks: 1, 
+  slam_dunks: 15}
+      }
+      }
+    },
   
-  away: {team_name: "Team Name", colors: [], players: {"Player 1": {number:, shoe:, points:, rebounds:, assists:, blocks:, slam_dunks:}}}
-  
+  away: {team_name: "Team Name", 
+  colors: [], 
+  players: 
+    {"Player 1": 
+    {   {"Alan Anderson": 
+    {number: 0, 
+      shoe: 16, 
+    points: 22, 
+  rebounds: 12, 
+   assists: 12, 
+     steals: 3,
+     blocks: 1, 
+ slam_dunks: 1}
+      }
+    },
+      }
+    }
 }
 
 end
-
-
-
-
-
