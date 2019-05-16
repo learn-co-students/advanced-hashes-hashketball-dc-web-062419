7805b682994d46f5de33eac05cf77767c01fed95
Trevahr Robertson <trevahr@yahoo.com>
2019-05-16 21:57:25 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,15 @@
 # Write your code here!
 
 
+def game_hash
+{
+  home: {team_name: "Team Name", colors: [], players: {"Player 1": {number:, shoe:, points:, rebounds:, assists:, blocks:, slam_dunks:}}}
+  
+  away: {team_name: "Team Name", colors: [], players: {"Player 1": {number:, shoe:, points:, rebounds:, assists:, blocks:, slam_dunks:}}}
+  
+}
 
-
-
+end
 
 
 
