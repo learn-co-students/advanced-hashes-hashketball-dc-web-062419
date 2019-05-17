6414bd56a5e7431c1237120b9f0d6e6756076d56
Trevahr Robertson <trevahr@yahoo.com>
2019-05-17 00:06:01 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -29,26 +29,77 @@ def game_hash
     assists: 10, 
      steals: 3,
      blocks: 1, 
-  slam_dunks: 15}
+  slam_dunks: 15},
+ "Mason Plumlee": 
+    {number: 1, 
+       shoe: 19, 
+     points: 26, 
+   rebounds: 12, 
+    assists: 6, 
+     steals: 3,
+     blocks: 8, 
+  slam_dunks: 5},
+ "Jason Terry": 
+    {number: 31, 
+       shoe: 15, 
+     points: 19, 
+   rebounds: 2, 
+    assists: 2, 
+     steals: 4,
+     blocks: 11, 
+  slam_dunks: 1}
       }
       }
     },
   
-  away: {team_name: "Team Name", 
-  colors: [], 
+  away: {team_name: "Charlotte Hornets", 
+  colors: ["Turquoise", "Purple"], 
   players: 
-    {"Player 1": 
-    {   {"Alan Anderson": 
-    {number: 0, 
-      shoe: 16, 
-    points: 22, 
-  rebounds: 12, 
-   assists: 12, 
+    {"Jeff Adrien": 
+    {number: 4, 
+      shoe: 18, 
+    points: 10, 
+  rebounds:  1, 
+   assists:  1, 
+     steals: 2,
+     blocks: 1, 
+ slam_dunks: 1},
+ "Bismack Biyombo": 
+    {number: 30, 
+       shoe: 14, 
+     points: 12, 
+   rebounds: 12, 
+    assists: 12, 
+     steals: 12,
+     blocks: 12, 
+  slam_dunks: 7},
+ "DeSagna Diop": 
+    {number: 11, 
+       shoe: 17, 
+     points: 17, 
+   rebounds: 19, 
+    assists: 10, 
      steals: 3,
      blocks: 1, 
- slam_dunks: 1}
-      }
-    },
+  slam_dunks: 15},
+ "Ben Gordonp": 
+    {number: 1, 
+       shoe: 19, 
+     points: 26, 
+   rebounds: 12, 
+    assists: 6, 
+     steals: 3,
+     blocks: 8, 
+  slam_dunks: 5},
+ "Brendan Haywood": 
+    {number: 31, 
+       shoe: 15, 
+     points: 19, 
+   rebounds: 2, 
+    assists: 2, 
+     steals: 4,
+     blocks: 11, 
+  slam_dunks: 1}
       }
     }
 }
